85d9eadc234c0539b874b7a37dff5e1ec65a0d5b
ender179 <kirilukvlad91@gmail.com>
2025-02-19 13:55:58 +0200

    Update countContacts.js

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,8 +1,22 @@
-import readContacts from '../utils/readContacts';  
+const fs = require('fs');
+const path = require('path');
 
-function countContacts() {  
-    const contacts = readContacts();  
-    console.log(`Кількість контактів: ${contacts.length}`);  
-}  
+const filePath = path.join(__dirname, 'src', 'db', 'db.json');
 
-countContacts();
+// Чтение файла с контактами
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Ошибка чтения файла:', err);
+    return;
+  }
+
+  let contacts = [];
+  try {
+    contacts = JSON.parse(data);
+  } catch (parseErr) {
+    console.error('Ошибка парсинга JSON:', parseErr);
+  }
+
+  // Вывод количества контактов в консоль
+  console.log(`Количество контактов: ${contacts.length}`);
+});
